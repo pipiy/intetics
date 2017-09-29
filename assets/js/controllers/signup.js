32d5d16a8058c0d7d8f1ac7280f28694aa50ba1d
Artsiom <artem.slabodnik@gmail.com>
2017-09-29 19:04:17 +0300

    sign in sign up form

---
+++ assets/js/controllers/signup.js
@@ -0,0 +1,10 @@
+app.controller("SignUpController", ['$scope', function($scope) {
+  $scope.signUp = function() {
+    debugger;
+    if ($scope.myForm.$valid) {
+      window.location.href = "signin.html";
+    } else {
+      $scope.error = true;
+    }
+  };
+}]);

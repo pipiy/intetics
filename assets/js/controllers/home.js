84be36a439bc863b1b60b229cd9a13941cc6c054
Artsiom <artem.slabodnik@gmail.com>
2017-10-05 13:47:11 +0300

    search input

---
+++ assets/js/controllers/home.js
@@ -0,0 +1,6 @@
+app.controller("HomeController", ['$scope', function($scope) {
+  $scope.images = [
+    {id: 1, src: "https://store.storeimages.cdn-apple.com/4974/as-images.apple.com/is/image/AppleInc/aos/published/images/I/MA/IMAC/IMAC?wid=874&hei=737&fmt=jpeg&qlt=95&op_sharpen=0&resMode=bicub&op_usm=0.5,0.5,0,0&iccEmbed=0&layer=comp&.v=dSMkx2"},
+    {id: 2, src: "https://support.apple.com/library/content/dam/edam/applecare/images/en_US/mac_apps/itunes/tvos10-itunes12-5-itunes-airplay-apple-tv-hero.jpg"}
+  ]
+}]);
--- assets/js/controllers/signup.js
+++ assets/js/controllers/signup.js
@@ -1,6 +1,5 @@
 app.controller("SignUpController", ['$scope', function($scope) {
   $scope.signUp = function() {
-    debugger;
     if ($scope.myForm.$valid) {
       window.location.href = "signin.html";
     }

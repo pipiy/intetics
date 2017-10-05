44fa8a9805f560886bb11c99233d147c0494d200
Artsiom <artem.slabodnik@gmail.com>
2017-10-05 14:26:09 +0300

    filterfilter a list in AngularJS using several links

--- assets/js/controllers/home.js
+++ assets/js/controllers/home.js
@@ -1,6 +1,34 @@
 app.controller("HomeController", ['$scope', function($scope) {
   $scope.images = [
-    {id: 1, src: "https://store.storeimages.cdn-apple.com/4974/as-images.apple.com/is/image/AppleInc/aos/published/images/I/MA/IMAC/IMAC?wid=874&hei=737&fmt=jpeg&qlt=95&op_sharpen=0&resMode=bicub&op_usm=0.5,0.5,0,0&iccEmbed=0&layer=comp&.v=dSMkx2"},
-    {id: 2, src: "https://support.apple.com/library/content/dam/edam/applecare/images/en_US/mac_apps/itunes/tvos10-itunes12-5-itunes-airplay-apple-tv-hero.jpg"}
+    {id: 1, src: "", name: "Man"},
+    {id: 2, src: "", name: "Man"},
+    {id: 3, src: "", name: "Man"},
+    {id: 4, src: "", name: "Computer"},
+    {id: 5, src: "", name: "Woman"},
+    {id: 6, src: "", name: "Woman"},
+    {id: 7, src: "", name: "Computer"},
+    {id: 8, src: "", name: "Work"},
+    {id: 9, src: "", name: "CIty"},
+    {id: 10, src: "", name: "Nature"},
+    {id: 11, src: "", name: "Nature"},
+    {id: 12, src: "", name: "Business"},
+    {id: 13, src: "", name: "Business"},
+    {id: 14, src: "", name: "Animals"},
+    {id: 15, src: "", name: "Animals"},
+    {id: 16, src: "", name: "Man"},
+    {id: 17, src: "", name: "Man"},
+    {id: 18, src: "", name: "Man"},
+    {id: 19, src: "", name: "Computer"},
+    {id: 20, src: "", name: "Woman"},
+    {id: 21, src: "", name: "Woman"},
+    {id: 22, src: "", name: "Computer"},
+    {id: 23, src: "", name: "Work"},
+    {id: 24, src: "", name: "CIty"},
+    {id: 25, src: "", name: "Nature"},
+    {id: 26, src: "", name: "Nature"},
+    {id: 27, src: "", name: "Business"},
+    {id: 28, src: "", name: "Business"},
+    {id: 29, src: "", name: "Animals"},
+    {id: 30, src: "", name: "Animals"}
   ]
 }]);
